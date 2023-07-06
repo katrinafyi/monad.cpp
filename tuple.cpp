4bde327f2e85686484e114542d0821d11071039a
rina <k@rina.fyi>
2023-07-06 22:46:15 +1000

    tuple_replace better type coercions.
    prefer to replace with the same type if possible.

--- tuple.cpp
+++ tuple.cpp
@@ -1,3 +1,5 @@
+#include <filesystem>
+#include <memory>
 #include <string>
 #include <variant>
 
@@ -5,12 +7,26 @@
 
 int main() {
   std::tuple<int, bool, std::string> asd{100, true, "hehehe"};
+  std::cout << typeid(asd).name() << std::endl;
   print_tuple(asd);
-  auto a = tuple_replace<1>(asd, false);
+
+  std::tuple<int, bool, std::string> a = tuple_replace<1>(asd, false);
+  std::cout << typeid(a).name() << std::endl;
   print_tuple(a);
-  a = tuple_replace<2>(a, "fsaji");
+
+  a = tuple_replace<2>(a, {"fsaji"});
+  std::cout << typeid(a).name() << std::endl;
   print_tuple(a);
-  a = tuple_map<2>(
-      a, [](const std::string &x) -> std::string { return x + " yoted asdf!"; });
-  print_tuple(a);
+
+  std::tuple<std::unique_ptr<std::string>, bool, std::string> aa =
+      tuple_replace<0>(a, std::make_unique<std::string>("yoted"));
+  std::cout << typeid(aa).name() << std::endl;
+  print_tuple(aa);
+
+  std::tuple<int, bool, std::filesystem::path> a2 =
+      tuple_map<2>(a, [](const std::string &x) -> std::filesystem::path {
+        return (std::filesystem::path{x} / " yoted asdf!");
+      });
+  std::cout << typeid(a2).name() << std::endl;
+  print_tuple(a2);
 }
--- tuple.hpp
+++ tuple.hpp
@@ -20,30 +20,55 @@ F conditional_v(T &&, F &&f) {
 template <std::size_t N, typename... Ts>
 using nth_t = typename std::tuple_element<N, std::tuple<Ts...>>::type;
 
-template <std::size_t i, typename T, typename... Ts, std::size_t... is>
-std::tuple<Ts...> tuple_replace_impl(const std::tuple<Ts...> &tuple, T &&item,
-                                     std::index_sequence<is...>) {
+template <std::size_t i, typename T, typename... Ts, std::size_t... is,
+          typename Tuple2 = std::tuple<std::conditional_t<i == is, T, Ts>...>>
+Tuple2 tuple_replace_impl(const std::tuple<Ts...> &tuple, T &&item,
+                          std::index_sequence<is...>) {
   return std::make_tuple(
       (conditional_v<i == is>(std::forward<T>(item), std::get<is>(tuple)))...);
 }
 
+template <std::size_t i, typename... Ts,
+          typename Is = std::index_sequence_for<Ts...>>
+auto tuple_replace(const std::tuple<Ts...> &tuple, nth_t<i, Ts...> &&item) {
+  return tuple_replace_impl<i, nth_t<i, Ts...>, Ts...>(
+      tuple, std::forward<nth_t<i, Ts...>>(item), Is{});
+}
+
 template <std::size_t i, typename T, typename... Ts,
           typename Is = std::index_sequence_for<Ts...>>
-  requires std::constructible_from<std::tuple_element_t<i, std::tuple<Ts...>>,
-                                   T>
-std::tuple<Ts...> tuple_replace(const std::tuple<Ts...> &tuple, T &&item) {
+auto tuple_replace(const std::tuple<Ts...> &tuple, T &&item) {
   return tuple_replace_impl<i, T, Ts...>(tuple, std::forward<T>(item), Is{});
 }
 
-template <std::size_t i, typename T, typename F, typename... Ts,
-          std::size_t... is, typename Tuple = std::tuple<Ts...>,
-          typename Tuple2 =
-              std::tuple<std::conditional_t<i == is, T, nth_t<is, Ts...>>...>>
-Tuple2 tuple_map_impl(const Tuple &tuple, F &&f,
-                      std::index_sequence<is...>...) {
+/*
+template <std::size_t i, typename T, typename... Ts, std::size_t... is,
+          typename Tuple2 = std::tuple<std::conditional_t<i == is, T, Ts>...>>
+Tuple2 tuple_emplace_impl(const std::tuple<Ts...> &tuple, T &&item,
+                          std::index_sequence<is...>) {
   return std::make_tuple(
-      (conditional_v<i == is>(f(std::get<i>(tuple)), std::get<is>(tuple)))...);
+      (conditional_v<i == is>(std::forward<T>(item), std::get<is>(tuple)))...);
 }
+
+template <std::size_t i, typename... Args, typename... Ts,
+          typename Is = std::index_sequence_for<Ts...>>
+  requires std::constructible_from<nth_t<i, Ts...>, Args...>
+auto tuple_emplace(const std::tuple<Ts...> &tuple, Args &&...item) {
+  return tuple_emplace_impl<i, nth_t<i, Ts...>, Ts...>(
+      tuple, {std::forward<Args>(item)...}, Is{});
+}
+*/
+
+// template <std::size_t i, typename T, typename F, typename... Ts,
+//           std::size_t... is, typename Tuple = std::tuple<Ts...>,
+//           typename Tuple2 = std::tuple<std::conditional_t<i == is, T,
+//           Ts>...>>
+// Tuple2 tuple_map_impl(const Tuple &tuple, F &&f,
+//                       std::index_sequence<is...>...) {
+//   return std::make_tuple(
+//       (conditional_v<i == is>(f(std::get<i>(tuple)),
+//       std::get<is>(tuple)))...);
+// }
 
 template <std::size_t i, typename F, typename... Ts,
           typename Tuple = std::tuple<Ts...>,
@@ -51,7 +76,8 @@ template <std::size_t i, typename F, typename... Ts,
           typename T =
               std::invoke_result_t<F, const std::tuple_element_t<i, Tuple> &>>
 auto tuple_map(const std::tuple<Ts...> &tuple, F &&f) {
-  return tuple_map_impl<i, T, F, Ts...>(tuple, std::forward<F>(f), Is{});
+  // return tuple_map_impl<i, T, F, Ts...>(tuple, std::forward<F>(f), Is{});
+  return tuple_replace<i, T, Ts...>(tuple, f(std::get<i>(tuple)));
 }
 
 template <class Tuple, std::size_t... I>
