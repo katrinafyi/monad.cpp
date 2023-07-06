4b8127ca810130fa5e578fde76226871aef2fb72
rina <k@rina.fyi>
2023-07-06 22:14:01 +1000

    tuple_map.

--- tuple.cpp
+++ tuple.cpp
@@ -5,7 +5,12 @@
 
 int main() {
   std::tuple<int, bool, std::string> asd{100, true, "hehehe"};
-  auto a = go<1>(asd, false);
-  print(asd);
-  print(a);
+  print_tuple(asd);
+  auto a = tuple_replace<1>(asd, false);
+  print_tuple(a);
+  a = tuple_replace<2>(a, "fsaji");
+  print_tuple(a);
+  a = tuple_map<2>(
+      a, [](const std::string &x) -> std::string { return x + " yoted asdf!"; });
+  print_tuple(a);
 }
--- tuple.hpp
+++ tuple.hpp
@@ -5,43 +5,62 @@
 #include <iostream>
 #include <tuple>
 
-template <std::size_t i, std::size_t is, typename Tuple,
-          typename T_is = std::tuple_element_t<is, Tuple>,
-          typename T_i = std::tuple_element_t<i, Tuple>>
-  requires(i != is)
-T_is g(const T_is &old, T_i &&) {
-  return old;
+template <bool cond, typename T, typename F>
+  requires cond
+T conditional_v(T &&t, F &&) {
+  return t;
 }
 
-template <std::size_t i, std::size_t is, typename Tuple,
-          typename T_is = std::tuple_element_t<is, Tuple>,
-          typename T_i = std::tuple_element_t<i, Tuple>>
-  requires(i == is)
-T_is g(const T_is &, T_i &&item) {
-  return item;
+template <bool cond, typename T, typename F>
+  requires(!cond)
+F conditional_v(T &&, F &&f) {
+  return f;
 }
+
+template <std::size_t N, typename... Ts>
+using nth_t = typename std::tuple_element<N, std::tuple<Ts...>>::type;
 
 template <std::size_t i, typename T, typename... Ts, std::size_t... is>
-std::tuple<Ts...> go2_impl(const std::tuple<Ts...> &tuple, T &&item,
-                           std::integer_sequence<std::size_t, is...>) {
-  return std::make_tuple((g<i, is, std::tuple<Ts...>>(
-      std::get<is>(tuple), std::forward<T>(item)))...);
+std::tuple<Ts...> tuple_replace_impl(const std::tuple<Ts...> &tuple, T &&item,
+                                     std::index_sequence<is...>) {
+  return std::make_tuple(
+      (conditional_v<i == is>(std::forward<T>(item), std::get<is>(tuple)))...);
 }
 
 template <std::size_t i, typename T, typename... Ts,
           typename Is = std::index_sequence_for<Ts...>>
-  requires std::convertible_to<T, std::tuple_element_t<i, std::tuple<Ts...>>>
-std::tuple<Ts...> go(const std::tuple<Ts...> &tuple, T &&item) {
-  return go2_impl<i, T, Ts...>(tuple, std::forward<T>(item), Is{});
+  requires std::constructible_from<std::tuple_element_t<i, std::tuple<Ts...>>,
+                                   T>
+std::tuple<Ts...> tuple_replace(const std::tuple<Ts...> &tuple, T &&item) {
+  return tuple_replace_impl<i, T, Ts...>(tuple, std::forward<T>(item), Is{});
 }
 
-template <class TupType, std::size_t... I>
-void print(const TupType &_tup, std::index_sequence<I...>) {
+template <std::size_t i, typename T, typename F, typename... Ts,
+          std::size_t... is, typename Tuple = std::tuple<Ts...>,
+          typename Tuple2 =
+              std::tuple<std::conditional_t<i == is, T, nth_t<is, Ts...>>...>>
+Tuple2 tuple_map_impl(const Tuple &tuple, F &&f,
+                      std::index_sequence<is...>...) {
+  return std::make_tuple(
+      (conditional_v<i == is>(f(std::get<i>(tuple)), std::get<is>(tuple)))...);
+}
+
+template <std::size_t i, typename F, typename... Ts,
+          typename Tuple = std::tuple<Ts...>,
+          typename Is = std::index_sequence_for<Ts...>,
+          typename T =
+              std::invoke_result_t<F, const std::tuple_element_t<i, Tuple> &>>
+auto tuple_map(const std::tuple<Ts...> &tuple, F &&f) {
+  return tuple_map_impl<i, T, F, Ts...>(tuple, std::forward<F>(f), Is{});
+}
+
+template <class Tuple, std::size_t... I>
+void print_tuple(const Tuple &_tup, std::index_sequence<I...>) {
   std::cout << "(";
   (..., (std::cout << (I == 0 ? "" : ", ") << std::get<I>(_tup)));
   std::cout << ")\n";
 }
 
-template <class... T> void print(const std::tuple<T...> &_tup) {
-  print(_tup, std::make_index_sequence<sizeof...(T)>());
+template <class... T> void print_tuple(const std::tuple<T...> &_tup) {
+  print_tuple(_tup, std::make_index_sequence<sizeof...(T)>());
 }
